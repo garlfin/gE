d8c0e81ee3369bbf853a5b887a617e32d3fc71d9
Griffin <sciontific1201@gmail.com>
2023-03-08 13:44:39 -0500

    Revert window size

--- gEC/main.cpp
+++ gEC/main.cpp
@@ -5,7 +5,7 @@ int main()
     if(gE::Init(4, 6)) return gE::FAIL;
 
     gE::Result result;
-    gE::DemoWindow window("Test DemoWindow", 1024, 1024, &result);
+    gE::DemoWindow window("Test DemoWindow", 1920, 1080, &result);
 
     if(result) return result;
 
